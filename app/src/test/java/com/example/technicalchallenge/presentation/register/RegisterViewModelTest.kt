13af72a7bcb5796991bb30b224e6e0308d0ccee1
yoelkill <elvis.yoelkill@gmail.com>
2024-09-11 02:52:44 -0500

    update RegisterViewModelTest

--- app/src/test/java/com/example/technicalchallenge/presentation/register/RegisterViewModelTest.kt
+++ app/src/test/java/com/example/technicalchallenge/presentation/register/RegisterViewModelTest.kt
@@ -6,10 +6,14 @@ import com.example.technicalchallenge.domain.useCases.RegisterClientUseCase
 import com.example.technicalchallenge.util.Resource
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.RelaxedMockK
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
@@ -21,10 +25,10 @@ import org.junit.Test
 @OptIn(ExperimentalCoroutinesApi::class)
 class RegisterViewModelTest{
 
-    @RelaxedMockK
+    @MockK
     private lateinit var repository: RegisterRepository
 
-    @RelaxedMockK
+    @MockK
     private lateinit var useCase: RegisterClientUseCase
 
 
@@ -34,9 +38,9 @@ class RegisterViewModelTest{
     @Before
     fun onBefore(){
         MockKAnnotations.init(this)
+        Dispatchers.setMain(UnconfinedTestDispatcher())
         useCase = RegisterClientUseCase(repository)
         viewModel = RegisterViewModel(useCase)
-        Dispatchers.setMain(Dispatchers.Unconfined)
 
     }
 
@@ -47,13 +51,19 @@ class RegisterViewModelTest{
 
     @Test
     fun `test registerClient`() = runTest {
-        val client = Client("John", "Doe", 30, "1990/01/01")
+        val client = Client("pepe", "Doe", 30, "1990/01/01")
         // Configurar el comportamiento del repositorio simulado
+
         coEvery{ repository.registerClient(any()) } returns flow {
             emit(Resource.Success(true))
         }
+
         viewModel.registerClient(client)
 
+        coVerify { repository.registerClient(any()) }
+
+        // Print the state for debugging
+        println("Current Register State: ${viewModel.registerState.value}")
         assert(viewModel.registerState.value is RegisterState.SUCCESS)
     }
 }
