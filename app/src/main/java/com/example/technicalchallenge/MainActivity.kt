e503b94bb0fd22aa47ecd20c742219a83b53a75b
yoelkill <elvis.yoelkill@gmail.com>
2024-09-11 03:01:49 -0500

    update mainactivity

--- app/src/main/java/com/example/technicalchallenge/MainActivity.kt
+++ app/src/main/java/com/example/technicalchallenge/MainActivity.kt
@@ -23,39 +23,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        FacebookSdk.sdkInitialize(applicationContext);
-        FirebaseApp.initializeApp(this)
         setContent {
             NavigationGraph()
         }
-
-    }
-
-    @Deprecated("This method has been deprecated in favor of using the Activity Result API\n      which brings increased type safety via an {@link ActivityResultContract} and the prebuilt\n      contracts for common intents available in\n      {@link androidx.activity.result.contract.ActivityResultContracts}, provides hooks for\n      testing, and allow receiving results in separate, testable classes independent from your\n      activity. Use\n      {@link #registerForActivityResult(ActivityResultContract, ActivityResultCallback)}\n      passing in a {@link StartActivityForResult} object for the {@link ActivityResultContract}.")
-    override fun startActivityForResult(intent: Intent, requestCode: Int) {
-        super.startActivityForResult(intent, requestCode)
-        Log.d( "entroo","aquii")
-    }
-
-
-    companion object {
-        const val RC_GOOGLE_SIGN_IN = 9001
-    }
-}
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TechnicalChallengeTheme {
 
     }
 }
